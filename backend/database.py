ec0edc6b66f1da6c2d0795b3ab37f5c7418d12e6
judeabdu <abdushakuluhssenkindu@gmail.com>
2026-05-26 12:43:48 +0300

    Final production PostgreSQL migration

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,19 @@
+import os
+
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
+
+from sqlalchemy.ext.declarative import declarative_base
+
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./goldbot.db"
 
-engine = create_engine(DATABASE_URL)
+DATABASE_URL = os.getenv(
+    "DATABASE_URL"
+)
+
+engine = create_engine(
+    DATABASE_URL
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
